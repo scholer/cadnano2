0d34a5cbe365b6c13260ff36b82df440347f4a51
Rasmus Scholer <rasserkrasser@hotmail.com>
2014-03-26 19:55:37 +0100

    Improvements to interactive mode of cadnanoqt.py; also changed app exec to prevent issue where cadnano would hang on Linux.

--- cadnanoqt.py
+++ cadnanoqt.py
@@ -28,11 +28,36 @@ Created by Jonathan deWerd on 2012-01-11.
 """
 import util, sys, os
 import cadnano
-from code import interact
+try:
+    ## If using IPython console, the IPython interpreter must be quit() before quitting cadnano.
+    ## EDIT: Using IPython is actually rather complicated (althrough easier in the newest dev version):
+    ## -- http://stackoverflow.com/questions/16737323/embedding-ipython-into-a-pyqt4-app
+    ## -- http://stackoverflow.com/questions/14739425/how-to-embed-ipython-kernel-into-pyqt4-program
+    ## -- http://ipython.org/ipython-doc/dev/interactive/qtconsole.html (note: it has to be dev; not in stable yet)
+    raise ImportError("IPython currently disabled, since it makes quitting super flaky.")
+    import IPython
+except ImportError:
+    print "(IPython not available, trying to import readline+rlcompleter...)"
+    IPython = None
+    try:
+        # On windows, readline can be provided via the pyreadline package (will create a 'readline' alias file during installation...)
+        import readline     # should be imported first, I believe...
+    except ImportError:
+        print "(readline/rlcompleter not available...)"
+        readline = None
+    else:
+        # If no ImportError...
+        import rlcompleter  # refers to readline during import. rlcompleter
+        # readline.parse_and_bind("tab: complete")
+
+#from code import interact
+import code
+
 util.qtWrapImport('QtGui', globals(),  ['qApp', 'QApplication', 'QIcon',\
                                         'QUndoGroup'])
 util.qtWrapImport('QtCore', globals(), ['QObject', 'QCoreApplication', 'Qt',\
                                         'QEventLoop', 'pyqtSignal'])
+#util.qtWrapImport('QtCore', globals(), ['SIGNAL', 'SLOT', 'pyqtSlot'])
 
 class CadnanoQt(QObject):
     dontAskAndJustDiscardUnsavedChanges = False
@@ -59,7 +84,6 @@ class CadnanoQt(QObject):
         self.partItem = None
         self.sharedApp = self
 
-
     def ignoreEnv(self):
         return os.environ.get('CADNANO_IGNORE_ENV_VARS_EXCEPT_FOR_ME', False)
 
@@ -70,10 +94,21 @@ class CadnanoQt(QObject):
         if os.environ.get('CADNANO_DEFAULT_DOCUMENT', False) and not self.ignoreEnv():
             self.sharedApp.shouldPerformBoilerplateStartupScript = True
         cadnano.loadAllPlugins()
+
+        ## NOTE: At this point, the qt application loop has not actually started.
+        ## It might be better (safer) to hook up a qtconsole
+        ## especially if loading ipython or doing other advanced stuff
+
         if "-i" in self.argv:
             print "Welcome to cadnano's debug mode!"
             print "Some handy locals:"
             print "\ta\tcadnano.app() (the shared cadnano application object)"
+
+            print "\tdc()\tthe document controller"
+            def dc():
+                # self.documentControllers is a set. This is an easy way to return an arbitrary element from a set:
+                return next(iter(self.documentControllers), None)
+
             print "\td()\tthe last created Document"
             def d():
                 return self.d
@@ -99,12 +134,43 @@ class CadnanoQt(QObject):
                 partitem = pi()
                 vHelix = vh(vhref)
                 return partitem.vhItemForVH(vHelix)
-                
+
+            def enableAutocomplete(ns):
+                readline.set_completer(rlcompleter.Completer(ns).complete)
+                readline.parse_and_bind("tab: complete")
+
             print "\tquit()\tquit (for when the menu fails)"
             print "\tgraphicsItm.findChild()  see help(pi().findChild)"
-            interact('', local={'a':self, 'd':d, 'w':w,\
-                                'p':p, 'pi':pi, 'vh':vh, 'vhi':vhi,\
-                                })
+            print "Local modules include: model, controllers, ui, views, data and plugins."
+            print "dc() controls file creation/loading/saving."
+            print "Use dc().openAfterMaybeSaveCallback(<filepath>) to open a new file."
+            print "If dc() is None, use a.newDocument([fp=filepath]) to create a new controller."
+            print "As always, dir() and help() are helpful."
+            print "For more detail, use inspect.getsource(<class or function>)"
+            print "Note that app quit/exit is a bit flaky when interactive mode is on."
+            print "This might be because the application loop has not actually been initiated."
+
+            ns = {'a':self, 'd':d, 'dc':dc, 'w':w, 'p':p, 'pi':pi, 'vh':vh, 'vhi':vhi,
+                  'enableAutocomplete':enableAutocomplete, 'stdQuit':quit, 'stdExit':exit}
+            if IPython:
+                print "Starting iPython embedded console...\n"
+                IPython.embed(user_ns=ns) # Take care, makes it hard to terminate the thread.
+                #IPython.embed_kernel(user_ns=ns) # Uh, not what you want... you need a qtconsole to connect.
+                #IPython.start_kernel(user_ns=ns) # Only available in ipython >0.13
+                #from IPython.frontend.terminal.embed import InteractiveShellEmbed
+                #self.ipshell = InteractiveShellEmbed()
+                #self.ipshell() # Still, if you first exit cadnano and then quit() in terminal the thread does not terminate.
+            elif readline:
+                print "Starting standard code.interact mode...\n"
+                readline.set_completer(rlcompleter.Completer(ns).complete) # Makes it work :)
+                readline.parse_and_bind("tab: complete")
+                code.interact('', local=ns)
+                #readline.set_completer(rlcompleter.Completer(ns).complete)
+                #readline.parse_and_bind("tab: complete")
+                print "Interactive mode complete..."
+            else:
+                print "No readline/completer available..."
+            print "Interactive mode complete!"
         # else:
         #     self.exec_()
 
@@ -113,16 +179,27 @@ class CadnanoQt(QObject):
                                                     "Maya", Qt.CaseInsensitive)
     def isGui(self):
         return True
-    
-    def exec_(self):
-        if hasattr(self, 'qApp'):
-            self.mainEventLoop = QEventLoop()
-            self.mainEventLoop.exec_()
-            #self.qApp.exec_()
 
-    def newDocument(self, isFirstNewDoc=False):
+    def exec_(self):
+        """
+        Starts main application loop. Invoked by the main.py bootstrapping script.
+        # Notice: CadnanoQt is NOT a QtApplication, it is a QtObject. self.qApp is the application.
+        """
+        if hasattr(self, 'qApp'):
+            #self.mainEventLoop = QEventLoop()
+            #self.connect(self.qApp, SIGNAL("lastWindowClosed()"), self.qApp, SLOT("quit()")) # added by RS - but doesn't work... and not required if using qApp.exec_()
+            #self.mainEventLoop.exec_()
+            self.qApp.exec_() # Yeah, this works much better than the above mainEventLoop.exec_(). Who added the extra QEventLoop code ??
+
+    def newDocument(self, isFirstNewDoc=False, fp=None):
+        """
+        Creates a new document, ensuring that a document controller is also instantiated if required.
+        If isFirstNewDoc is True, a new documentController is always instantiated.
+        A new documentController will create a new document during __init__().
+        This method is called by finishInit() to ensure a document is created on application init.
+        """
         from controllers.documentcontroller import DocumentController
-        defaultFile = os.environ.get('CADNANO_DEFAULT_DOCUMENT', None)
+        defaultFile = fp or os.environ.get('CADNANO_DEFAULT_DOCUMENT', None)
         if defaultFile and isFirstNewDoc:
             defaultFile = path.expanduser(defaultFile)
             defaultFile = path.expandvars(defaultFile)
@@ -132,13 +209,12 @@ class CadnanoQt(QObject):
             decode(doc, file(defaultFile).read())
             print "Loaded default document: %s" % doc
         else:
-            docCtrlrCount = len(self.documentControllers)
-            if docCtrlrCount == 0:  # first dc
-                # dc adds itself to app.documentControllers
+            dc = next(iter(self.documentControllers), None)
+            if dc: # dc already exists
+                dc.newDocument()
+            else: # first dc
+                # dc creates a new doc during init and adds itself to app.documentControllers:
                 dc = DocumentController()
-            elif docCtrlrCount == 1:  # dc already exists
-                dc = list(self.documentControllers)[0]
-                dc.newDocument()  # tell it to make a new doucment
         return dc.document()
 
     def prefsClicked(self):
